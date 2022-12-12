01b9dd1b61b8efdf47d8cbab7d183dd673ffe187
andy3o0 <mianmuhmmadusman@gmail.com>
2022-12-13 02:15:52 +0500

    push

--- pages/index.jsx
+++ pages/index.jsx
@@ -75,9 +75,9 @@ const Home = () => {
             </a>
           </div>)}
           {name && (<div className='text-black w-full text-5xl absolute bottom-5 z-10 flex items-center justify-center  gap-5'>
-            <a className='flex items-center' href={`https://www.facebook.com/sharer/sharer.php?u=https://xmas.mktimothy.com/?name=${name}`}><button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">FACEBOOK</button>
+            <a className='flex items-center' href={`https://www.facebook.com/sharer/sharer.php?u=https://greeting-andy3o0.vercel.app/?name=${name}`}><button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">FACEBOOK</button>
             </a>
-            <a className='flex items-center' href={`whatsapp://send?text=https://xmas.mktimothy.com/?name=${name}`}><button className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">WHATSAPP</button>
+            <a className='flex items-center' href={`whatsapp://send?text=https://greeting-andy3o0.vercel.app/?name=${name}`}><button className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">WHATSAPP</button>
             </a>
           </div>)}
           <header className="bg-red-500 w-full backdrop-brightness-100 px-5 h-[50px] absolute flex justify-between items-center top-0">
@@ -178,9 +178,9 @@ const Home = () => {
             </a>
           </div>)}
           {name && (<div className='text-black text-5xl absolute bottom-5 z-10 flex items-center justify-center  gap-5'>
-            <a className='flex items-center' href={`https://www.facebook.com/sharer/sharer.php?u=https://xmas.mktimothy.com/?name=${name}`}><button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">FACEBOOK</button>
+            <a className='flex items-center' href={`https://www.facebook.com/sharer/sharer.php?u=https://greeting-andy3o0.vercel.app/?name=${name}`}><button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">FACEBOOK</button>
             </a>
-            <a className='flex items-center' href={`whatsapp://send?text=https://xmas.mktimothy.com/?name=${name}`}><button className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">WHATSAPP</button>
+            <a className='flex items-center' href={`whatsapp://send?text=https://greeting-andy3o0.vercel.app/?name=${name}`}><button className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">WHATSAPP</button>
             </a>
           </div>)}
 
